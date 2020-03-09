95386ffc730de6a4d0c03ee835521a9004ea0240
NNaattaa <50961364+NNaattaa@users.noreply.github.com>
2020-03-09 21:07:21 +0200

    edit

--- homework-05/task-04.js
+++ homework-05/task-04.js
@@ -1,27 +1,20 @@
 'use strict';
 class StringBuilder {
-  constructor(string) {
-    this._value = string;
+  constructor(value) {
+    this._value = value;
   }
-  // - возвращает текущее значение поля _value
   get value() {
     return this._value;
   }
-
-  //   - получает парметр str (строку) и добавляет ее в конец value
   append(str) {
     this._value = this._value + str;
-  } //Метод
-
-  //  - получает парметр str (строку) и добавляет ее в начало value
+  }
   prepend(str) {
     this._value = str + this._value;
-  } //Метод
-
-  //  - получает парметр str (строку) и добавляет ее в начало и в конец value
+  }
   pad(str) {
     this._value = str + this._value + str;
-  } //Метод
+  }
 }
 
 const builder = new StringBuilder('.');
